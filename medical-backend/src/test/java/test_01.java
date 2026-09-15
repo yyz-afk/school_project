2651c2f7e7d6a6ad8a2738f3b561114e25ac0d4c
yyz <2161672768@qq.com>
2026-09-15 17:58:44 +0800

    build: 添加test依赖以及目录

---
+++ medical-backend/src/test/java/test_01.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author yyz
+ * @date 2026/9/15 17:56
+ */
+
+@SpringBootTest
+public class test_01 {
+
+
+    @Test
+    public void test_ch1() {
+        String fileName = "1234567";
+        String name = fileName.substring(0, 13) + "|||||||||";
+        System.out.println(name);
+    }
+
+
+}
