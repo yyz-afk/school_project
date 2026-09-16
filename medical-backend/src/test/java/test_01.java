b9595f5501de360f2189f3add22bf427f4a5229e
yyz <2161672768@qq.com>
2026-09-16 08:56:14 +0800

    test:test

--- medical-backend/src/test/java/test_01.java
+++ medical-backend/src/test/java/test_01.java
@@ -17,4 +17,12 @@ public class test_01 {
         System.out.println(name);
     }
 
+    @Test
+    public void test_ch2() {
+        String[] test = new String[]{"111", "222", "333"};
+        for (int i = 0; i < 10; i++) {
+            System.out.println(test[i]);
+        }
+    }
+
 }
